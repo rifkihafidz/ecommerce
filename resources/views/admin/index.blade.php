87f1fef5e64bbd337da3e15d63dbe5f9d5e89740
unknown <wildprojr31@gmail.com>
2020-10-13 14:24:22 +0700

    Commit 1

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard Admin') }}</div>
+                <div class="card-header">{{ __('Dashboard Administrator') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
